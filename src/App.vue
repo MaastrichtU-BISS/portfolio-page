ab9ec0aa756f0421db65bbc5135311d5991235cf
CvdL-UM <c.vanderlans@maastrichtuniversity.nl>
2022-10-25 11:42:38 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="background-images">
+      <img src="./assets/img/biss_um_logo.png" id="biss-logo" />
+      <img src="./assets/img/hexagon.png" class="hexagon" />
+    </div>
+    <div class="row">
+      <Title />
+    </div>
+    <div class="row">
+      <ul class="grid remove-list-styling">
+        <li class="placeholder">Placeholder</li>
+        <li class="placeholder">Placeholder</li>
+        <li class="placeholder">Placeholder</li>
+        <li class="placeholder">Placeholder</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Title from "./components/Title.vue";
+</script>
---
+++ src/components/Title.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1></h1>
+</template>
+
+<style scoped lang="scss">
+@import 'src/assets/typed';
+
+h1 {
+  @include typed("Hey you", "Yeah, you!", "Come checkout BISS<[100]>", (pause-typed: 4), (caret-width: 4px));
+}
+</style>
