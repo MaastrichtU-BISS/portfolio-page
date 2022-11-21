474ec9783ecac43e80a0656dddaeaf72a2f2207b
Carlos <carloseam94@gmail.com>
2022-11-21 15:25:54 +0100

    3 cards with modals+iframes

--- src/App.vue
+++ src/App.vue
@@ -8,19 +8,63 @@
       <Title />
     </div>
     <div class="row">
-      <ul class="grid remove-list-styling">
-        <li class="placeholder">Placeholder</li>
-        <li class="placeholder">Placeholder</li>
-        <li class="placeholder">Placeholder</li>
-        <li class="placeholder">Placeholder</li>
-      </ul>
+      <div class="cards-list">
+        <a id="modal1" href="#au2s" class="card 1">
+          <div class="card_image"> <img src="./assets/img/au2sem.gif"/> </div>
+          <div class="card_title title-white">
+            <p>Audio to Semantics</p>
+          </div>
+        </a>
+      </div>
+      <div class="cards-list">
+        <a id="modal2" href="#proj-ov" class="card 2">
+          <div class="card_image"> <img src="./assets/img/projects.gif"/> </div>
+          <div class="card_title title-white">
+            <p>Our Projects</p>
+          </div>
+        </a>
+      </div>
+      <div class="cards-list">
+        <a id="modal3" href="#txt2img" class="card 3">
+          <div class="card_image"> <img src="./assets/img/dolly.gif"/> </div>
+          <div class="card_title title-white">
+            <p>Text to Images</p>
+          </div>
+        </a>
+      </div>
+        <!-- <a id="modal1" href="#au2s">DEMO1</a>
+        <a id="modal2" href="#proj-ov">DEMO2</a>
+        <a id="modal3" href="#txt2img">DEMO3</a> -->
     </div>
     <div class="row fixed-bottom" style="padding: 0 .5rem;">
       <h2>*BISS is like the A-team, but with professors from Maastricht University</h2>
+    </div>
+    <div>
+      <InnerModal :id="'au2s'" :url="'http://localhost:3001'"></InnerModal>
+      <InnerModal :id="'proj-ov'" :url="'http://localhost:3000'"></InnerModal>
+      <InnerModal :id="'txt2img'" :url="'http://localhost:3002'"></InnerModal>
     </div>
   </div>
 </template>
 
 <script setup>
 import Title from "./components/Title.vue";
+import InnerModal from "./components/InnerModal.vue"
+import {ref, reactive, onMounted} from 'vue';
+
+const selectedModal = ref(1);
+
+onMounted(()=>{
+  // { color: 'red' }
+  $("#modal1").animatedModal();
+  $("#modal2").animatedModal();
+  $("#modal3").animatedModal();
+})
+
+
 </script>
+<style>
+@import "../src/assets/css/animate.min.css";
+@import "../src/assets/css/close-modal.css";
+@import "../src/assets/css/cards.css"
+</style>
---
+++ src/components/InnerModal.vue
@@ -0,0 +1,23 @@
+<template>
+    <div :id="id">
+          <div :class="'close-' + id"> 
+            <div class="close-container">
+              <div class="leftright"></div>
+              <div class="rightleft"></div>
+              <label class="close">close</label>
+            </div>
+          </div>
+          <div class="modal-content">
+              <iframe :src="url"></iframe>
+          </div>
+      </div>
+</template>
+<script setup>
+  const props = defineProps(["id", "url"]);
+</script>
+<style>
+ iframe {
+  width: 100%;
+  height: auto;
+ }
+</style>
