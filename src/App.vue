1e8568038524c8a86fde24af35f5fcb66dcfc3e4
CvdL-UM <c.vanderlans@maastrichtuniversity.nl>
2023-05-09 11:06:01 +0200

    Changed text display

--- src/App.vue
+++ src/App.vue
@@ -7,27 +7,36 @@
     <div class="nieuws">
       <div class="text-container">
         <p>
-          Wil jij weten hoe je data voor jouw organisatie kunt laten werken? Heb
-          je een idee voor een nieuwe toepassing? Wil je weten wat BISS precies
-          doet? Schrijf je dan in voor een spreekuur!
+          Wil jij weten hoe je data voor jouw organisatie kunt laten werken? Heb je een
+          idee voor een nieuwe toepassing? Wil je weten wat BISS precies doet? Schrijf je
+          dan in voor een spreekuur!
         </p>
         <p>
-          Brightlands Institute for Smart Society (BISS) is een
-          onderzoeksinstituut van de Universiteit Maastricht. BISS helpt
-          organisaties met innovatieve verantwoorde oplossingen voor data
-          vraagstukken. Bij BISS benaderen we data vraagstukken vanuit een multi
-          disciplinaire aanpak. Wil je hier meer over weten? Of heb je een
-          concrete vraag? Ga dan in gesprek met onze onderzoekers tijdens het
-          spreekuur op vrijdagen in het BISS kantoor (online is natuurlijk ook
-          mogelijk) op de campus in Heerlen.
+          Brightlands Institute for Smart Society (BISS) is een onderzoeksinstituut van de
+          Universiteit Maastricht. BISS helpt organisaties met innovatieve verantwoorde
+          oplossingen voor data vraagstukken. Bij BISS benaderen we data vraagstukken
+          vanuit een multi disciplinaire aanpak.
         </p>
         <p>
-          Wat? Spreekuur met een onderzoeker van BISS over datascience en
-          Artificial Intelligence Voor wie? Iedereen die meer wil weten of
-          datascience en de BISS aanpak Wanneer? Op vrijdagen live op de
-          Brightlands Smart Services Campus in Heerlen of online Hoe? Loop
-          binnen tussen 11 en 12 uur op vrijdagen
+          Meer weten of heb je een concrete vraag? Ga dan in gesprek met
+          onze onderzoekers tijdens het spreekuur op vrijdagen in het BISS kantoor (online
+          is natuurlijk ook mogelijk) op de campus in Heerlen.
         </p>
+        <ul style="padding-inline-start: 20px">
+          <li>
+            Wat? Spreekuur met een onderzoeker van BISS over datascience en Artificial
+            Intelligence
+          </li>
+          <br />
+          <li>Voor wie? Iedereen die meer wil weten of datascience en de BISS aanpak</li>
+          <br />
+          <li>
+            Wanneer? Op vrijdagen live op de Brightlands Smart Services Campus in Heerlen
+            of online
+          </li>
+          <br />
+          <li>Hoe? Loop binnen tussen 11:00 en 12:00 uur op vrijdagen</li>
+        </ul>
       </div>
       <!-- <Title /> -->
     </div>
