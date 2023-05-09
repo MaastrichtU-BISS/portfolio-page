98a14d5598f107819648881e46f07cf08f03e735
CvdL-UM <c.vanderlans@maastrichtuniversity.nl>
2023-05-09 11:12:37 +0200

    Extract news to component

--- src/App.vue
+++ src/App.vue
@@ -4,42 +4,7 @@
       <img src="./assets/img/biss_um_logo.png" id="biss-logo" />
       <img src="./assets/img/hexagon.png" class="hexagon" />
     </div>
-    <div class="news">
-      <div class="text-container">
-        <p>
-          Wil jij weten hoe je data voor jouw organisatie kunt laten werken? Heb je een
-          idee voor een nieuwe toepassing? Wil je weten wat BISS precies doet? Schrijf je
-          dan in voor een spreekuur!
-        </p>
-        <p>
-          Brightlands Institute for Smart Society (BISS) is een onderzoeksinstituut van de
-          Universiteit Maastricht. BISS helpt organisaties met innovatieve verantwoorde
-          oplossingen voor data vraagstukken. Bij BISS benaderen we data vraagstukken
-          vanuit een multi disciplinaire aanpak.
-        </p>
-        <p>
-          Meer weten of heb je een concrete vraag? Ga dan in gesprek met
-          onze onderzoekers tijdens het spreekuur op vrijdagen in het BISS kantoor (online
-          is natuurlijk ook mogelijk) op de campus in Heerlen.
-        </p>
-        <ul style="padding-inline-start: 20px">
-          <li>
-            Wat? Spreekuur met een onderzoeker van BISS over datascience en Artificial
-            Intelligence
-          </li>
-          <br />
-          <li>Voor wie? Iedereen die meer wil weten of datascience en de BISS aanpak</li>
-          <br />
-          <li>
-            Wanneer? Op vrijdagen live op de Brightlands Smart Services Campus in Heerlen
-            of online
-          </li>
-          <br />
-          <li>Hoe? Loop binnen tussen 11:00 en 12:00 uur op vrijdagen</li>
-        </ul>
-      </div>
-      <!-- <Title /> -->
-    </div>
+    <News />
     <div class="row">
       <div class="cards-list">
         <Card
@@ -95,7 +60,7 @@
 @import "../src/assets/css/cards.css";
 </style>
 <script setup>
-import Title from "./components/Title.vue";
+import News from "./components/News.vue";
 import Card from "./components/Card.vue";
 import InnerModal from "./components/InnerModal.vue";
 import { onMounted, ref, computed } from "vue";
